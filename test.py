608706c0c9691a22a7fe5ccdcd257a3b5b55164d
angryhors <groznykon@gmail.com>
2019-12-10 13:35:24 +0100

    color change

--- test.py
+++ test.py
@@ -7,13 +7,13 @@ import newton
 
 # Set simulation parameters.
 window_size = [1080, 720]
-gravity = [0, 10]
+gravity = [0, 5]
 dampening = 0.95
 bg_color = (33, 33, 33)
-ball_color = (76, 175, 80)
+ball_color = (255, 235, 59)
 fps = 60
 dt = 1/fps
-number_of_balls = 32
+number_of_balls = 64
 
 # Initialize the environment.
 env = newton.Environment(window_size, gravity, dampening, dt)
@@ -23,16 +23,16 @@ screen = pygame.display.set_mode((window_size[0], window_size[1]))
 
 def generate_balls(number_of_balls):
     for ball in range(number_of_balls):
-        radius = np.random.randint(20, 40)
+        radius = np.random.randint(15, 30)
         coordinates = np.random.randint(radius, np.array(window_size) - radius, 2)
-        velocity = np.random.randint(-6, 6, 2)
+        velocity = np.random.randint(-20, 20, 2)
         mass = np.random.randint(40, 60)
         env.objects.append((newton.Ball(env, coordinates, velocity, radius, mass)))
 
 def draw_env(env):
     screen.fill(bg_color)
     for i in env.objects:
-        pygame.draw.circle(screen, ball_color, (int(i.x), int(i.y)), i.radius)
+        pygame.draw.circle(screen, ball_color, (int(i.x), int(i.y)), i.radius, 1)
     pygame.display.flip()
 
 # Run the simulation.
